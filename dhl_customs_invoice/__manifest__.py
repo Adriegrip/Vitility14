d8ef7bbfb5a446b552490c5edc8846786b28bd9a
Hajaj Roshan <hajaj@cybrosys.in>
2021-04-07 13:52:01 +0530

    [FIX]dhl_customs_invoice origin_country_id

--- dhl_customs_invoice/__manifest__.py
+++ dhl_customs_invoice/__manifest__.py
@@ -7,10 +7,11 @@
     'category': 'Warehouse',
     'version': '14.0.1.0.1',
     'license': 'AGPL-3',
-    'depends': [
-        'dhl_delivery_label',
-        'base_dhl'
-    ],
+    'depends': ['base',
+                'dhl_delivery_label',
+                'base_dhl',
+                'account_intrastat'
+                ],
     'data': [
         'security/ir.model.access.csv',
         'views/report_customer_invoice_dhl.xml',
